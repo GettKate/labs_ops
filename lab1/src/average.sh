1a447d63aa763f5a788c161c63013b81ed2db74d
GettKate <94016791+GettKate@users.noreply.github.com>
2023-12-10 22:13:28 +0000

    .

--- lab1/src/average.sh
+++
@@ -1,9 +0,0 @@
-sum=0
-count=0
-for var in "$@"; do
-    sum=$(($sum + $var))
-    count=$(($count + 1))
-done
-
-echo "average - $((sum / $count))"
-echo "count - $count"
--- lab1/src/numb_gen.sh
+++
@@ -1,4 +0,0 @@
-for i in {1..150}
-do
-    od -An -N1 -i /dev/random >> numbers.txt
-done
